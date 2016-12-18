4e4ebb42edc526b3a031f86deebea9856a151d7d
kfzx-qiusd <qiusd@sdc.icbc.com.cn>
2016-12-18 18:23:23 +0800

    内部类II

--- src/innerClass/InnerClassTest.java
+++ src/innerClass/InnerClassTest.java
@@ -7,11 +7,15 @@ public class InnerClassTest {
 	public static void main(String[] args) {
 
 		TalkingClock clock = new TalkingClock(1000, true);
-		clock.start();
+		// 普通内部类
+		// clock.start();
+		// 局部内部类
+		clock.start_inner(2000, true);
+		clock.start_anonymous_inner(3000, true);
 		
 		JOptionPane.showMessageDialog(null, "Quit？");
 		System.exit(0);
-		
+
 	}
 
 }
