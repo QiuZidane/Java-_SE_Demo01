12c691119546b34d779208cd54dec56382fc1f72
QZidane <qiusdsdc@126.com>
2017-02-01 20:50:41 +0800

    加入json-java和gson的jar包

---
+++ src/json/GsonTest.java
@@ -0,0 +1,21 @@
+package json;
+
+import com.google.gson.Gson;
+
+public class GsonTest {
+	
+	private static void createByBean() {
+		
+		
+		
+		Gson gson = new Gson();
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
